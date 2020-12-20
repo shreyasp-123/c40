db29687152bbcb59d767263fe754f335b0597ee4
shreyasp-123 <71993552+shreyasp-123@users.noreply.github.com>
2020-12-20 15:51:45 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var database
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var form, game, player;
+var car1, car2, car3, car4, cars;
+var car1Img, car2Img, car3Img, car4Img, trackImg
+var finishedPlayers = 0
+var playerStatusFinished
+
+function preload(){
+    car1Img = loadImage("images/car1.png")
+    car2Img = loadImage("images/car2.png")
+    car3Img = loadImage("images/car3.png")
+    car4Img = loadImage("images/car4.png")
+    trackImg = loadImage("images/track.jpg")
+}
+
+function setup(){
+    createCanvas(displayWidth - 20,displayHeight - 30);
+
+    database = firebase.database()
+    console.log(database)
+    game = new Game()
+    game.getState()
+    game.start()
+}
+
+function draw(){
+
+    if(playerCount === 4 && finishedPlayers === 0){
+        game.update(1)
+
+    }
+    if(gameState === 1){
+        game.play();
+    }
+    if(gameState === 2){
+        game.displayRank()
+    }
+    if(finishedPlayers === 4){
+        game.update(2)
+    }
+    
+}
